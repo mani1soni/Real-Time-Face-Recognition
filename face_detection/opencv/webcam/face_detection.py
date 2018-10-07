5087617fe202ab7f5248960f339e9b9cf19551b4
shravankumar9892 <shravankumarshetty9892@gmail.com>
2018-10-07 23:47:12 +0530

    documentation cleaned

--- face_detection/opencv/webcam/face_detection.py
+++ face_detection/opencv/webcam/face_detection.py
@@ -1,19 +1,14 @@
 import cv2
 import sys
 
-# Initializing the face cascade from the xml file
-# given
-# Cascade is just an xml file containing face
-# detection data
+# Initializing the face cascade from the xml file given
+# Cascade is just an xml file containing face detection data
 cascPath = "../haarcascade_frontalface_default.xml"
 faceCascade = cv2.CascadeClassifier(cascPath)
 
-# To capture a video, you need to create a VideoCapture object.
-# Its argument can be either the device index or the name of a video 
-# file. Device index is just the number to specify which camera. 
-# Normally one camera will be connected (as in my case). So I simply 
-# pass 0 (or -1). You can select the second camera by passing 1 and 
-# so on. After that, you can capture frame-by-frame. 
+# To capture a video, you need to create a VideoCapture object. Its argument can be either the device index or the name of a video 
+# file. Device index is just the number to specify which camera.  Normally one camera will be connected (as in my case). So I simply 
+# pass 0 (or -1). You can select the second camera by passing 1 and  so on. After that, you can capture frame-by-frame. 
 # But at the end, don’t forget to release the capture.
 
 # This line sets the video source to the default webcam, which OpenCV can easily capture.
@@ -21,23 +16,17 @@ faceCascade = cv2.CascadeClassifier(cascPath)
 video_capture = cv2.VideoCapture(0)
 # video_capture = cv2.VideoCapture(<file_name>)
 
-# A video file can also be passed here, but you need to have 
-# ffmpeg installed for that since OpenCV itself cannot decode
-# compressed video. Ffmpeg acts as the front end for OpenCV, 
-# and, ideally, it should be compiled directly into OpenCV. 
+# A video file can also be passed here, but you need to have ffmpeg installed for that since OpenCV itself cannot decode
+# compressed video. Ffmpeg acts as the front end for OpenCV, and, ideally, it should be compiled directly into OpenCV. 
 # This is not easy to do, especially on Windows.
 
 while True:
     # Capture frame-by-frame
-    # video_capture.read() returns actual video frame read, in 
-    # each loop ( as we are inside a while loop) and a return code
-    # which is a bool(True | False). 
-    # If frame is read correctly, return value will be True.
-    ret, frame = video_capture.read()
+    # video_capture.read() returns actual video frame read, in each loop ( as we are inside a while loop) and a return code
+    # which is a bool(True | False). If frame is read correctly, return value will be True.
     
-
+    ret, frame = video_capture.read()
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-
     faces = faceCascade.detectMultiScale(
         gray,
         scaleFactor=1.1,
@@ -52,8 +41,7 @@ while True:
     # Display the resulting frame
     cv2.imshow('Video', frame)
     
-    # 0xFF == ord('q') simply means script will exit if we press
-    # q key.
+    # 0xFF == ord('q') simply means script will exit if we press q key.
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
--- face_detection/opencv/without_webcam/face_detection.py
+++ face_detection/opencv/without_webcam/face_detection.py
@@ -1,26 +1,19 @@
-# Installing OpenCV is the hardest part as 
-# there are several compatibility issues, 
-# i.e 32/64 bit differences, other libraries 
-# could be of older versions, etc.
+# Installing OpenCV is the hardest part as there are several compatibility issues, 
+# i.e 32/64 bit differences, other libraries could be of older versions, etc.
 import cv2
 import sys
 
-# Passing image and cascade names as command-line arguments
-# imagePath should be dir location of the image you want detection for.
+# Passing image and cascade names as command-line arguments imagePath should be dir location of the image you want detection for.
 imagePath = sys.argv[1]
 
 # For face detection keep:
 cascPath = '../haarcascade_frontalface_default.xml'
 
-# Initializing the face cascade from the xml file
-# given via the arguments in cascPath.
-# Cascade is just an xml file containing face
-# detection data
+# Initializing the face cascade from the xml file given via the arguments in cascPath.
+# Cascade is just an xml file containing face detection data.
 faceCascade = cv2.CascadeClassifier(cascPath)
 
-# Read the image. Many operations in OpenCV 
-# are done in grayscale.
-# Remember OpenCV loads images as BGR images.
+# Read the image. Many operations in OpenCV are done in grayscale. Remember OpenCV loads images as BGR images.
 image = cv2.imread(imagePath)
 image_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
 gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
@@ -42,8 +35,7 @@ faces = faceCascade.detectMultiScale(
 # The function returns a list of rectangles in which it believes it found a face.
 print("Found {0} faces!".format(len(faces)))
 
-# Next, we will loop over where it thinks it found something and
-# draw a rectangle around the faces
+# Next, we will loop over where it thinks it found something and draw a rectangle around the faces
 for (x, y, w, h) in faces:
     cv2.rectangle(image_rgb, (x, y), (x+w, y+h), (0, 255, 0), 2)
 
